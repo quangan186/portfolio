867b9189febbdeff0345940fae74a508fdb55d97
quangan186 <61352012+quangan186@users.noreply.github.com>
2023-11-12 14:27:19 +0700

    fix: download file

--- src/components/contact/Contacts.tsx
+++ src/components/contact/Contacts.tsx
@@ -3,7 +3,7 @@ import Title from '../utilities/text/Title'
 import {BsArrowUpCircle, BsArrowDownCircle} from 'react-icons/bs'
 import Text from '../utilities/text/Text'
 const Contacts: React.FC = () => {
-  const [isDisplayed, setIsDisplayed] = useState(true)
+  const [isDisplayed, setIsDisplayed] = useState(false)
   return (
     <div className={`fixed bottom-0 w-full ${isDisplayed ? "translate-y-0": "translate-y-full"}`}>
         <div className={`w-full h-fit flex justify-end  text-white px-6 ${isDisplayed ? "translate-y-0": "-translate-y-full"}`}>
@@ -15,7 +15,7 @@ const Contacts: React.FC = () => {
         </div>
         <div className={`bg-[#323B4C] w-full flex justify-between items-center border-y border-black py-2 px-8 `}>
           <Title className='!text-[20px] text-[#FFFBF5]'>Get my CV:</Title>
-          <a href='/public/quangan-cv.pdf' download='Bui Quang An - Web developer.pdf' className='bg-[#FFFBF5] inline-flex items-center justify-center rounded-lg w-[150px] text-[#323B4C] h-[50px]'>
+          <a href='/quangan-cv.pdf' download='Bui Quang An - Web developer.pdf' className='bg-[#FFFBF5] inline-flex items-center justify-center rounded-lg w-[150px] text-[#323B4C] h-[50px]'>
             Download here
           </a>
         </div>
