ce16c1bdc3609420d355f0462a99209d87fed5a1
An Bui <anbui@imt-soft.com>
2024-04-27 17:18:52 +0700

    update cert and CV

--- src/constants/educations.tsx
+++ src/constants/educations.tsx
@@ -34,6 +34,36 @@ export const certificates = [
         time: 'April 2024',
         link: 'https://coursera.org/share/6a8d1bd62196d45d2e6889b51da434d3'
     },
+    {
+        title: 'Programming with JavaScript',
+        from: 'Coursera',
+        time: 'April 2024',
+        link: 'https://www.coursera.org/account/accomplishments/verify/UC22F9FXUYRF?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course'
+    },
+    {
+        title: 'React Native',
+        from: 'Coursera',
+        time: 'April 2024',
+        link: 'https://coursera.org/share/7a8bded29b18fcbdd55f639a0fab15ab'
+    },
+    {
+        title: 'React Basics',
+        from: 'Coursera',
+        time: 'April 2024',
+        link: 'https://coursera.org/share/31467517274c57882643c231e6f52c4e'
+    },
+    {
+        title: 'Introduction to Mobile Development',
+        from: 'Coursera',
+        time: 'April 2024',
+        link: 'https://coursera.org/share/0fc13208fe3e6d366dc5a5a49bf69062'
+    },
+    {
+        title: 'Introduction to VueJS Framework',
+        from: 'Coursera',
+        time: 'April 2024',
+        link: 'https://coursera.org/share/b1d21f996fb9cc13bbff32b7795e6f91'
+    },
 ]
 
 export const extraActivities = [
